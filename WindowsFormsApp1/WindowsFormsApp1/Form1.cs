43d9081fe44f4bf4172dfc21a0fd281677d5b8e1
LouiisFOI <lboutry23@student.foi.hr>
2023-05-06 16:15:06 +0200

    #5 Creation of the main

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,30 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string oSelectedFile = "";
+            System.Windows.Forms.OpenFileDialog oDlg = new System.Windows.Forms.OpenFileDialog();
+            if (System.Windows.Forms.DialogResult.OK == oDlg.ShowDialog())
+            {
+                UserFileReader reader = new UserFileReader();
+                UserInfoForm info = new UserInfoForm();
+                User user = new User();
+                UserParser parser = new UserParser();
+                UserDisplayer displayer = new UserDisplayer();
+
+                reader.Path = oDlg.FileName;
+                reader.ReadFile();
+                user = parser.ParseUser(reader.FileLine);
+                displayer.DisplayUserInfo(user, info);
+                displayer.UpdateTextContent(this, info);
+
+
+            }
+        }
+
+        
+
     }
 }
