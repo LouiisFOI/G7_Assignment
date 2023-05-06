34eabbd0050a8a5313e208544b76a873f6d85138
LouiisFOI <lboutry23@student.foi.hr>
2023-05-06 16:05:13 +0200

    #2 Implementation of UserParser class

---
+++ WindowsFormsApp1/WindowsFormsApp1/UserParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class UserParser
+    {
+        public User ParseUser(string[] fileContent)
+        {
+            List<string> content = fileContent.ToList();
+            content.RemoveAt(content.Count - 1);
+
+            User user = new User();
+
+            foreach (var (line, i) in content.Select((line, i) => (line, i)))
+            {
+
+                string name = line.Split('=')[0];
+                string value = line.Split('=')[1];
+
+
+
+                user.dataList.Add(new FileData(name, value));
+            }
+
+            return user;
+        }
+    }
+}
