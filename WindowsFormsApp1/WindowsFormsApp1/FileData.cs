f6e5a9aaa1dfc68a61ae8a8ec3f9e93c23313eb5
LouiisFOI <lboutry23@student.foi.hr>
2023-05-06 15:41:26 +0200

    #2 Creation of a File Data class

---
+++ WindowsFormsApp1/WindowsFormsApp1/FileData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class FileData
+    {
+        public string name;
+        public string value;
+
+        public FileData()
+        {
+
+        }
+        public FileData(string Name, string Value)
+        {
+            name = Name;
+            value = Value;
+        }
+
+    }
+}
