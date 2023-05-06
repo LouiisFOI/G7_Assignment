2d8b256fa1ec603e3f2cc527a51b9794745219ac
LouiisFOI <lboutry23@student.foi.hr>
2023-05-06 16:04:35 +0200

    #2 Creation of class UserInfoForm

---
+++ WindowsFormsApp1/WindowsFormsApp1/UserInfoForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class UserInfoForm
+    {
+        public FileData name = new FileData("Full Name", "");
+        public FileData yearOfBirth = new FileData("Year Of Birth", "");
+        public FileData city = new FileData("City", "");
+        public FileData faculty = new FileData("Faculty", "");
+        public FileData role = new FileData("Role", "");
+        public FileData specificAttribute = new FileData("[role specific attribute]", "");
+    }
+}
